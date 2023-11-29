c1c15d55f933fb128a33beec807fc8d68a2d7bfd
Owner <Owner@DESKTOP-GHBRFIE>
2023-11-29 11:15:14 -0500

    Add project files.

---
+++ Final Project/Data/Spell.cs
@@ -0,0 +1,15 @@
+﻿namespace Final_Project.Data
+{
+    public class Spell
+    {
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public string Description { get; set; }
+        public string Range { get; set; }
+        public string Components { get; set; }
+        public string Duration { get; set; }
+        public bool Concentration { get; set; }
+        public string CastingTime { get; set; }
+
+    }
+}
---
+++ Final Project/Services/ISpellsService.cs
@@ -0,0 +1,9 @@
+using Final_Project.Data;
+
+namespace Final_Project.Services
+{
+    public interface ISpellsService
+    {
+        public List<Spell> Spellbook { get; set; }  
+    }
+}
