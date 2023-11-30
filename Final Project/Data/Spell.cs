e3faeec6f27ac845f2ba943f118a01ed2500c106
Ethan Roberts <KianBanana@yahoo.com>
2023-11-30 11:47:45 -0500

    (Roughly) Working

--- Final Project/Data/Spell.cs
+++ Final Project/Data/Spell.cs
@@ -3,12 +3,12 @@
     public class Spell
     {
         public string Name { get; set; }
-        public int Level { get; set; }
+        public string Level { get; set; }
         public string Description { get; set; }
         public string Range { get; set; }
         public string Components { get; set; }
         public string Duration { get; set; }
-        public bool Concentration { get; set; }
+        public string Concentration { get; set; }
         public string CastingTime { get; set; }
 
     }
